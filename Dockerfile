4505573ae7692b5f227f389dc5dcd8deb7bf427a
Micah Galizia <micahgalizia@gmail.com>
2021-12-12 23:22:53 -0500

    ok first working bits

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3.9-alpine
 
-RUN pip install requests
+RUN python -m pip install requests beautifulsoup4
 
 COPY screening.py ./
 
--- screening.py
+++ screening.py
@@ -1,12 +1,54 @@
 """Docker script to automate answering the TVDSB covid screen utility."""
 import os
 import sys
+import requests
+from bs4 import BeautifulSoup
 
 MAIN_PAGE = 'https://schoolapps2.tvdsb.ca/students/covid_screening_verification/index.aspx'
+USER_INPUT = 'txtUsername'
+PASS_INPUT = 'txtPassword'
 
 
 def screen(user, pwd):
-    print('Screening {}:{}'.format(user,pwd))
+    """Screen a user."""
+    print('Screening {}:{}'.format(user, pwd))
+
+    sess = requests.Session()
+    resp = sess.get(MAIN_PAGE)
+    if not resp.status_code == 200:
+        print('HTTPS status {} when fetching {}'.format(req.status, MAIN_PAGE))
+        print('Response content: {}'.format(resp.content))
+        sys.exit(1)
+
+    soup = BeautifulSoup(resp.content, features="html.parser")
+
+    form = soup.find('form')
+    inputs = filter(lambda inp: not inp['id'] == 'rdoOptions_1', form.find_all('input'))
+    form_vals = {inp['name']: inp['value'] if inp.has_attr('value') else None for inp in inputs}
+    if USER_INPUT in form_vals:
+        form_vals[USER_INPUT] = user
+    if PASS_INPUT in form_vals:
+        form_vals[PASS_INPUT] = pwd
+
+    resp = sess.post(MAIN_PAGE, data=form_vals)
+    soup = BeautifulSoup(resp.content, features="html.parser")
+    error_span = soup.find('span', id='lblError')
+    error = "" if error_span is None else error_span.text
+    if len(error) > 0:
+        print('Page responds with error: "{}"'.format(error))
+        sys.exit(1)
+
+    form = soup.find('form')
+    inputs = filter(lambda inp: not inp['id'] == 'rdoOptions_1', form.find_all('input'))
+    form_vals = {inp['name']: inp['value'] if inp.has_attr('value') else None for inp in inputs}
+    if USER_INPUT in form_vals:
+        form_vals[USER_INPUT] = user
+    if PASS_INPUT in form_vals:
+        form_vals[PASS_INPUT] = pwd
+
+    resp = sess.post(MAIN_PAGE, data=form_vals)
+    soup = BeautifulSoup(resp.content, features="html.parser")
+    print(soup.find('p').text)
 
 
 if 'USER' not in os.environ:
@@ -24,5 +66,5 @@ if len(users) != len(passes):
     print('Length for users and passes different. Please consult documentation.')
     sys.exit(1)
 
-for user, pwd in dict(zip(users, passes)).items():
-    screen(user, pwd)
+for k, v in dict(zip(users, passes)).items():
+    screen(k, v)
