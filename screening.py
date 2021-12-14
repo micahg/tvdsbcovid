6952d3aae90e756e9a6a0bfbc0b4b0272943aa98
Micah Galizia <micahgalizia@gmail.com>
2021-12-13 20:50:10 -0500

    optimize

--- screening.py
+++ screening.py
@@ -8,11 +8,14 @@ MAIN_PAGE = 'https://schoolapps2.tvdsb.ca/students/covid_screening_verification/
 USER_INPUT = 'txtUsername'
 PASS_INPUT = 'txtPassword'
 
+FORM_ANSWER_NO = 'rdoOptions_1'
+PAGE_SPAN_ERR_ID = 'lblError'
+
 
 def process_form_soup(soup, user, pwd):
     """Process HTML for values."""
     form = soup.find('form')
-    inputs = filter(lambda inp: not inp['id'] == 'rdoOptions_1', form.find_all('input'))
+    inputs = filter(lambda inp: not inp['id'] == FORM_ANSWER_NO, form.find_all('input'))
     form_vals = {inp['name']: inp['value'] if inp.has_attr('value') else None for inp in inputs}
     if USER_INPUT in form_vals:
         form_vals[USER_INPUT] = user
@@ -21,44 +24,38 @@ def process_form_soup(soup, user, pwd):
     return form_vals
 
 
-def screen(user, pwd):
-    """Screen a user."""
-    print('Screening {}:{}'.format(user, pwd))
-
-    sess = requests.Session()
-    resp = sess.get(MAIN_PAGE)
+def request_page(sess, data=None):
+    """Request the page."""
+    resp = sess.get(MAIN_PAGE) if data is None else sess.post(MAIN_PAGE, data=data)
     if not resp.status_code == 200:
         print('HTTPS status {} when fetching {}'.format(resp.status, MAIN_PAGE))
         print('Response content: {}'.format(resp.content))
         sys.exit(1)
 
     soup = BeautifulSoup(resp.content, features="html.parser")
-
-    form_vals = process_form_soup(soup, user, pwd)
-
-    resp = sess.post(MAIN_PAGE, data=form_vals)
-    if not resp.status_code == 200:
-        print('HTTPS status {} when fetching {}'.format(resp.status, MAIN_PAGE))
-        print('Response content: {}'.format(resp.content))
-        sys.exit(1)
-
-    soup = BeautifulSoup(resp.content, features="html.parser")
-    error_span = soup.find('span', id='lblError')
+    error_span = soup.find('span', id=PAGE_SPAN_ERR_ID)
     error = "" if error_span is None else error_span.text
     if len(error) > 0:
         print('Page responds with error: "{}"'.format(error))
         sys.exit(1)
 
+    return soup
+
+
+def screen(user, pwd):
+    """Screen a user."""
+    print('Screening {}:{}'.format(user, pwd))
+
+    sess = requests.Session()
+    soup = request_page(sess)
+
     form_vals = process_form_soup(soup, user, pwd)
+    soup = request_page(sess, form_vals)
 
-    resp = sess.post(MAIN_PAGE, data=form_vals)
-    if not resp.status_code == 200:
-        print('HTTPS status {} when fetching {}'.format(resp.status, MAIN_PAGE))
-        print('Response content: {}'.format(resp.content))
-        sys.exit(1)
+    form_vals = process_form_soup(soup, user, pwd)
+    soup = request_page(sess, form_vals)
 
-    soup = BeautifulSoup(resp.content, features="html.parser")
-    print(soup.find('p').text)
+    print('Result is "{}"'.format(soup.find('p').text))
 
 
 if 'USER' not in os.environ:
